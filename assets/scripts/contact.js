845c4f154ce10a5614520a8526499a26c472a95c
calvinbareja89 <calvinbareja89@gmail.com>
2021-10-19 19:09:24 +0800

    Editted Contact Page - Incorporated Bootstrap

--- assets/scripts/contact.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector(".nav-toggle");
-const sideBar = document.querySelector(".my-sidebar");
-
-toggleBtn.addEventListener('click', function () {
-	sideBar.classList.toggle('show-sidebar');
-});
